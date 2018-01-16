af20bda3c7b12f1e5919483569d8ba434f5ae4a7
zhangll <zhangll@309099cc-2552-4745-813b-332d872b3f5f>
2018-01-16 07:36:49 +0000

    执行文件的部分提交

--- test/src/main/java/com/zll/GenMain.java
+++ test/src/main/java/com/zll/GenMain.java
@@ -1,6 +1,9 @@
 package com.zll;
 
 import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.api.ProgressCallback;
+import org.mybatis.generator.api.ShellCallback;
+import org.mybatis.generator.api.VerboseProgressCallback;
 import org.mybatis.generator.config.Configuration;
 import org.mybatis.generator.config.xml.ConfigurationParser;
 import org.mybatis.generator.internal.DefaultShellCallback;
@@ -10,22 +13,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GenMain {
-	public static void main(String[] args) {
-		//读取文件
-		File configFile = new File(GenMain.class.getResource("/generatorConfig.xml").getFile());
-		List<String> warnings = new ArrayList<String>();
-		ConfigurationParser cp = new ConfigurationParser(warnings);
-		Configuration config = null;
-		//true:覆盖生成
-		DefaultShellCallback callback = new DefaultShellCallback(true);
-		MyBatisGenerator myBatisGenerator = null;
-		try {
-			config = cp.parseConfiguration(configFile);
-			myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
-			myBatisGenerator.generate(null);
-			System.err.println("代码成功生成!");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    public static void main(String[] args) {
+        boolean overwrite = true;
+        //读取文件
+        File configFile = new File(GenMain.class.getResource("/generatorConfig.xml").getFile());
+        List<String> warnings = new ArrayList<String>();
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = null;
+        //true:覆盖生成
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = null;
+        ProgressCallback progressCallback = new VerboseProgressCallback();
+        try {
+            config = cp.parseConfiguration(configFile);
+            myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+            myBatisGenerator.generate(progressCallback);
+            System.err.println("代码成功生成!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
